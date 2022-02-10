840c7c0ab05e755d09d3cb8f3201192854521472
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-10 14:26:39 +0900

    deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module RailsApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # deviseの日本語化
+    config.i18n.default_locale = :ja
   end
 end
