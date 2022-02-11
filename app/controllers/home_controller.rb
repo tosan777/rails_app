61a77d12ed22bb347e8362e599b949c10fbad45d
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-11 20:52:10 +0900

    destyle.cssの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  # リクエストしてきたユーザーを認証する。
+  # ユーザーがログイン済みの場合はアクセスを許可して、未ログインの場合はroot_pathにリダイレクトする。
+  before_action :authenticate_user!
+
   def index
   end
 end
