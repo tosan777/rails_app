98d8a8ad9a59df36e601e59564f94634b68ffae2
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-11 18:19:16 +0900

    deviseの追加＆セットアップ

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # deviseのデフォルトURLを設定
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
