f3e99c323f6e6b9c6ac3528b1684461cbad38578
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-11 17:43:25 +0900

    devise関連アンインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # topページの指定
+  root 'top#index'
 
   # get 'URLの設定', to: '〇〇コントローラーの＃〇〇アクションを呼び出す'
   get 'tweets', to: 'tweets#index'
--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class UserTest < ActiveSupport::TestCase
+class TopControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
