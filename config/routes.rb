3eb2213297f6ac87a57c756c33c8a38282161991
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-18 23:41:33 +0900

    device.scssの追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
   # topページの指定
   root to: 'home#index'
+
 
   # get 'URLの設定', to: '〇〇コントローラーの＃〇〇アクションを呼び出す'
   get 'tweets', to: 'tweets#index'
