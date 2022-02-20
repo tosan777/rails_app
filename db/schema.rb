a8bd740f83fb3a32f9743017875b82d89b1de553
Kosuke Toyama <80808445+tosan777@users.noreply.github.com>
2022-02-20 15:25:18 +0900

    書き込みモデルPostの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_132309) do
+ActiveRecord::Schema.define(version: 2022_02_20_062412) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "topic_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
@@ -26,9 +35,10 @@ ActiveRecord::Schema.define(version: 2022_02_12_132309) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name", default: "", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "topics"
 end
